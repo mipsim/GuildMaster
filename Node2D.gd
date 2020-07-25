bca827f8c75ec9cb055e6285ed971114f819cba5
LukeKnezCK <54819450+LukeKnezCK@users.noreply.github.com>
2020-07-24 20:39:04 -0700

    mouse cursor stuff
    sample cursor & sound

---
+++ Node2D.gd
@@ -0,0 +1,13 @@
+extends Node2D
+
+
+func _ready():
+	Input.set_mouse_mode(Input.MOUSE_MODE_HIDDEN)
+
+
+func _process(delta):
+	self.position = self.get_global_mouse_position()
+	
+	if Input.is_action_just_pressed("left_mouse"):
+		$ClickSound.play()
+
